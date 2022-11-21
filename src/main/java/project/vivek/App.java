3d892c34cc58ec1fa93ae72041d6cd732e64e894
Ananth <ananth@gmail.com>
2022-11-21 14:34:38 +0530

    asdf

---
+++ src/main/java/project/vivek/App.java
@@ -0,0 +1,13 @@
+package project.vivek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
